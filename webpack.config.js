f42a0b37a8e69c27a1cb6e2a9d51a48530336b4d
SofaLis <94624304+SofaLis@users.noreply.github.com>
2022-05-20 19:50:21 +0300

    Pr9

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, 'index.html'),
+          template: path.resolve(__dirname, 'src', 'index.html'),
           cache: false,
         }),
         new MiniCssExtractPlugin(),
